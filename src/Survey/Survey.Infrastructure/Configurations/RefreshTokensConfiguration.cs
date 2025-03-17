3698f171b9917a48197fbdd5a4c51b6975a5d92a
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-17 19:32:06 +0200

    feat : handle model configs

---
+++ src/Survey/Survey.Infrastructure/Configurations/RefreshTokensConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Survey.Domain.Models.Identity;
+using Survey.Domain.ValueObjects.Identity;
+
+namespace Survey.Infrastructure.Configurations
+{
+    public class RefreshTokensConfiguration : IEntityTypeConfiguration<UserRefreshTokens>
+    {
+        public void Configure(EntityTypeBuilder<UserRefreshTokens> builder)
+        {
+            builder.HasKey(t => t.Id).HasName("rt_id");
+            builder.Property(rt => rt.Id)
+                .HasConversion(
+                    rtId => rtId.Value,
+                    dbId => RefreshTokenId.Of(dbId)
+                );
+
+            builder.HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(rt => rt.UserId)
+                .IsRequired();
+
+            builder.ComplexProperty(
+                rt => rt.UserMetaData,
+                metaBuilder =>
+                {
+                    metaBuilder.Property(m => m.CountryCode);
+                    metaBuilder.Property(m => m.Country);
+                    metaBuilder.Property(m => m.City);
+                    metaBuilder.Property(m => m.UserAgent);
+                    metaBuilder.Property(m => m.IP);
+                    metaBuilder.Property(m => m.LocationLat);
+                    metaBuilder.Property(m => m.LocationLng);
+                    metaBuilder.Property(m => m.TimeZone);
+                }
+
+            );
+        }
+    }
+}
---
+++ src/Survey/Survey.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Survey.Domain.Models.Identity;
+using Survey.Domain.ValueObjects.Identity;
+
+namespace Survey.Infrastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasKey(user => user.Id);
+
+            builder.HasMany(user => user.RefreshTokens)
+                .WithOne(rt => rt.User)
+                .HasPrincipalKey(user => user.Id)
+                .HasForeignKey(rt => rt.UserId);
+        }
+    }
+}
