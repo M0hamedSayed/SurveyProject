89f4010128fa7d10849d9840abf9d7ae133ac4d6
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-17 19:34:28 +0200

    feat : handle event dispatcher

---
+++ src/Survey/Survey.Domain/Events/Dispatcher/IDomainEventDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using Survey.Domain.Interfaces.Models;
+
+namespace Survey.Domain.Events.Dispatcher
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(IDomainEvent domainEvent);
+    }
+}
---
+++ src/Survey/Survey.Infrastructure/Events/DomainEventDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Survey.Domain.Events.Dispatcher;
+using Survey.Domain.Interfaces.Models;
+
+namespace Survey.Infrastructure.Events
+{
+    public class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        private readonly IMediator _mediator;
+
+        public DomainEventDispatcher(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task DispatchAsync(IDomainEvent domainEvent)
+        {
+            await _mediator.Publish(domainEvent);
+        }
+    }
+}
