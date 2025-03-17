9d2dfefa70847136e352b41779880f8ddfe2dd9f
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-17 19:36:32 +0200

    feat : handle di

--- src/Survey/Survey.API/Program.cs
+++ src/Survey/Survey.API/Program.cs
@@ -1,3 +1,6 @@
+using Survey.Application;
+using Survey.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DependencyInjection
+builder.Services.AddApplicationServices()
+    .AddInfrastructureService(builder.Configuration);
 
 var app = builder.Build();
 
--- src/Survey/Survey.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Survey.Application
-{
-    public class Class1
-    {
-
-    }
-}
