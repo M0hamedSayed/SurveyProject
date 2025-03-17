8c2a5e9e77c1750370efd8451411aaee0e42eae3
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-17 19:32:47 +0200

    feat : db connection

---
+++ src/Survey/Survey.Infrastructure/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Survey.Domain.Models.Identity;
+using Survey.Domain.ValueObjects.Identity;
+
+namespace Survey.Infrastructure.DatabaseContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<ApplicationUser> applicationUsers { get; set; }
+        public DbSet<ApplicationRole> applicationRoles { get; set; }
+        public DbSet<UserRefreshTokens> userRefreshTokens { get; set; }
+
+    }
+}
