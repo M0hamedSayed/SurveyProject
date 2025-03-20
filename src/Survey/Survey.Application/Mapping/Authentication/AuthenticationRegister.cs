90885452cec600e651a2a580256100e767a87f09
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-20 21:00:59 +0200

    feat : handle auth mapping

---
+++ src/Survey/Survey.Application/Mapping/Authentication/AuthenticationRegister.cs
@@ -0,0 +1,12 @@
+﻿using Mapster;
+
+namespace Survey.Application.Mapping.Authentication
+{
+    public partial class AuthenticationRegister : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            LoginMapping(config);
+        }
+    }
+}
---
+++ src/Survey/Survey.Application/Mapping/Authentication/Commands/LoginMapping.cs
@@ -0,0 +1,26 @@
+﻿using Mapster;
+using Survey.Application.Features.Authentication.Commands.Login;
+using Survey.Domain.Models.Identity;
+
+namespace Survey.Application.Mapping.Authentication
+{
+    public partial class AuthenticationRegister
+    {
+        public void LoginMapping(TypeAdapterConfig config)
+        {
+            config.NewConfig<ApplicationUser, LoginResult>()
+                .Map(dest => dest.activeSession, src => src.RefreshTokens.FirstOrDefault().Adapt<ActiveSession>());
+
+            config.NewConfig<UserRefreshTokens, ActiveSession>()
+            .Map(dest => dest.AccessToken, src => src.AccessToken)
+            .Map(dest => dest.UserAgent, src => src.UserMetaData.UserAgent)
+            .Map(dest => dest.IP, src => src.UserMetaData.IP)
+            .Map(dest => dest.Country, src => src.UserMetaData.Country)
+            .Map(dest => dest.CountryCode, src => src.UserMetaData.CountryCode)
+            .Map(dest => dest.City, src => src.UserMetaData.City)
+            .Map(dest => dest.TimeZone, src => src.UserMetaData.TimeZone)
+            .Map(dest => dest.LocationLat, src => src.UserMetaData.LocationLat)
+            .Map(dest => dest.LocationLng, src => src.UserMetaData.LocationLng);
+        }
+    }
+}
