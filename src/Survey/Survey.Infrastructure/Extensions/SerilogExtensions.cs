50f4eb0731f10bc297a3c4d6fb0be8aa24b3ab4b
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-20 21:03:33 +0200

    feat : serialog config

---
+++ src/Survey/Survey.Infrastructure/Extensions/SerilogExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Survey.Infrastructure.Extensions
+{
+    public static class SerilogExtensions
+    {
+        public static IHostBuilder UseSerilogLogging(this IHostBuilder hostBuilder)
+        {
+            return hostBuilder
+                .UseSerilog((context, configuration) =>
+            {
+                configuration
+                    .ReadFrom.Configuration(context.Configuration); // Read settings from appsettings.json
+            });
+        }
+    }
+}
