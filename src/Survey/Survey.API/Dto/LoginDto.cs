cb6875dbcf902b20d126950854d100074ac4b98c
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-03-20 21:05:38 +0200

    feat : first stage of mapping in api

---
+++ src/Survey/Survey.API/Dto/LoginDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Survey.API.Dto
+{
+    public class LoginDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ src/Survey/Survey.API/Mapping/AuthMapping.cs
@@ -0,0 +1,15 @@
+﻿using Mapster;
+using Survey.API.Dto;
+using Survey.Application.Features.Authentication.Commands.Login;
+
+namespace Survey.API.Mapping
+{
+    public class AuthMapping : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<LoginDto, LoginCommand>()
+                .RequireDestinationMemberSource(true);
+        }
+    }
+}
