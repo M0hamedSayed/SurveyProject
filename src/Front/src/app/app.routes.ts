9049d28eba42c168ba48fc813c9274a5f22e5a4d
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-04-13 09:57:55 +0200

    fix : app routes

--- src/Front/src/app/app.routes.ts
+++ src/Front/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { SurveysListComponent } from './pages/surveys-list/surveys-list.component';
 import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SurveyComponent } from './pages/survey/survey.component';
+import { adminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'survey', pathMatch: 'full' },
   { path: 'survey', component: SurveysListComponent },
-  { path: 'survey/:id', component: SurveysListComponent },
-  { path: 'survey/create', component: CreateSurveyComponent },
+  {
+    path: 'survey/create',
+    component: CreateSurveyComponent,
+    canActivate: [adminGuard],
+  },
+  { path: 'survey/:id', component: SurveyComponent },
   { path: '**', component: NotFoundComponent },
 ];
