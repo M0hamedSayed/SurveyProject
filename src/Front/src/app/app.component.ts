d945e7fd8fa80a6c48093b6905cec7148c3e812e
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-04-13 10:00:41 +0200

    feat : apply notification

--- src/Front/src/app/app.component.ts
+++ src/Front/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CreateSurveyComponent } from './pages/create-survey/create-survey.component';
-import { SurveysListComponent } from './pages/surveys-list/surveys-list.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CreateSurveyComponent, SurveysListComponent],
+  imports: [RouterOutlet, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
