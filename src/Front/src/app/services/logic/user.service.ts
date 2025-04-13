b20b7857f5daf99490db35c6559ed33719ef0762
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2025-04-13 09:53:28 +0200

    feat : handle user data

--- src/Front/src/app/services/logic/user.service.ts
+++ src/Front/src/app/services/logic/user.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { GlobalState } from '../../common/classes/globalState';
+import { IUser } from '../../common/Interfaces/IUser';
+import { SurveyApiService } from '../api/survey-api.service';
+import { catchError, Observable, of, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UserService {
+export class UserService extends GlobalState<IUser> {
+  private _api = inject(SurveyApiService);
 
-  constructor() { }
+  constructor() {
+    super();
+  }
+
+  InitializeUserData(): Observable<any> {
+    return this._api.getMe().pipe(
+      tap((value: any) => {
+        if (value?.Data) {
+          this.setState(value?.Data);
+        }
+      }),
+      catchError((err) => {
+        console.log(err);
+        return of(null);
+      })
+    );
+  }
 }
